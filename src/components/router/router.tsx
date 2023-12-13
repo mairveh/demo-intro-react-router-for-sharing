8ed1b34da933e669501f6ac354f200fe76374cc7
Merve <babutine@yahoo.com>
2023-12-13 20:48:49 +0000

    fix: replace product with products component

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../home/home";
 import Product from "../products/product/product";
+import Products from "../products/products";
 import About from "../about/about";
 import Help from "../help/help";
 import NotFound from "../not_found/not_found";
@@ -10,7 +11,7 @@ const Router: React.FC = () => (
   <Routes>
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Home />} />
-      <Route path="/products" element={<Product />} />
+      <Route path="/products" element={<Products />} />
       <Route path="/products/:productId" element={<Product />} />
       <Route path="/about" element={<About />} />
       <Route path="/help" element={<Help />} />
